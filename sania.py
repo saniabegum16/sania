fbe291c3da6ccd8cf582a91ac1cd1617dfb69024
saniabegum16 <saniabegum598@gmail.com>
2025-04-12 01:55:44 -0700

    Add files via upload

---
+++ sania.py
@@ -0,0 +1,7 @@
+#phython program to print numbers in triangular form using for loop
+rows=int(input("Enter number of rows:"))
+for i in range(rows,0,-1):
+    print("\n")
+    for j in range(rows-i+1):
+        print(i,"",end="")
+   
