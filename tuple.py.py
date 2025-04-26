8b7b451c98e0003791f7f21626bb6805444d97c2
saniabegum16 <saniabegum598@gmail.com>
2025-04-26 14:16:38 +0530

    Add files via upload

---
+++ tuple.py.py
@@ -0,0 +1,7 @@
+from array import array
+my_list=[1,2,3,4,5]
+array_from_list=array('i',my_list)
+my_tuple=(6,7,8,9,10)
+array_from_tuple=array('i',my_tuple)
+print("Array from list:",array_from_list)
+print("Array from tuple:",array_from_tuple)
